211a8e2c8b55b3f2ac087166f3538892cce93c0c
Mrbysco <Mrbysco@users.noreply.github.com>
2022-07-13 03:00:38 +0200

    Update forge [push]

--- src/main/java/com/mrbysco/measurements/client/LoginHandler.java
+++ src/main/java/com/mrbysco/measurements/client/LoginHandler.java
@@ -5,12 +5,12 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class LoginHandler {
 	@SubscribeEvent
-	public void onLogIn(ClientPlayerNetworkEvent.LoggedInEvent event) {
+	public void onLogIn(ClientPlayerNetworkEvent.LoggingIn event) {
 		ClientHandler.clear();
 	}
 
 	@SubscribeEvent
-	public void onLogOut(ClientPlayerNetworkEvent.LoggedOutEvent event) {
+	public void onLogOut(ClientPlayerNetworkEvent.LoggingOut event) {
 		ClientHandler.clear();
 	}
 }
